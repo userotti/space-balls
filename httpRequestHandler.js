767a20111f1058777255194abb40d3601bc103f5
christian <cfrauenstein@gmail.com>
2021-03-03 16:16:18 +0200

    weird bug where io is not always defined, my custom routing broke

--- httpRequestHandler.js
+++ httpRequestHandler.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 
 //Main request handler
 module.exports = function (req, res) {
-  console.log("req.url: ", req.url);
-  if (req.url === '/socket.io.js'){
+  console.log("req.url.includes('socket'): ", req.url.includes('socket'));
+  if (req.url.includes('socket')){
       fs.readFile('./node_modules/socket.io/client-dist/socket.io.js', function(error, data) {  
       if (error) {  
         res.writeHead(404);  
@@ -12,22 +12,6 @@ module.exports = function (req, res) {
       } else {  
         res.writeHead(200, {  
           'Content-Type': 'application/javascript'  
-        });  
-        res.write(data);  
-        res.end();  
-      }  
-    });
-  }
-
-  if (req.url === '/socket.io.js.map'){
-    fs.readFile('./node_modules/socket.io/client-dist/socket.io.js.map', function(error, data) {  
-      if (error) {  
-        res.writeHead(404);  
-        res.write(error);  
-        res.end();  
-      } else {  
-        res.writeHead(200, {  
-          'Content-Type': 'application/json'  
         });  
         res.write(data);  
         res.end();  
