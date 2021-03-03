1e7bd8bea5243402086ab7ec36f574da7868a46d
christian <cfrauenstein@gmail.com>
2021-03-03 15:02:38 +0200

    got the socker stuff working

---
+++ httpRequestHandler.js
@@ -0,0 +1,50 @@
+var fs = require('fs');  
+
+//Main request handler
+module.exports = function (req, res) {
+  if (req.url === '/socket.io.js'){
+      fs.readFile('./node_modules/socket.io/client-dist/socket.io.js', function(error, data) {  
+      if (error) {  
+        res.writeHead(404);  
+        res.write(error);  
+        res.end();  
+      } else {  
+        res.writeHead(200, {  
+          'Content-Type': 'application/javascript'  
+        });  
+        res.write(data);  
+        res.end();  
+      }  
+    });
+  }
+
+  if (req.url === '/socket.io.js.map'){
+    fs.readFile('./node_modules/socket.io/client-dist/socket.io.js.map', function(error, data) {  
+      if (error) {  
+        res.writeHead(404);  
+        res.write(error);  
+        res.end();  
+      } else {  
+        res.writeHead(200, {  
+          'Content-Type': 'application/json'  
+        });  
+        res.write(data);  
+        res.end();  
+      }  
+    });
+  }
+
+  fs.readFile('./page.html', function(error, data) {  
+      if (error) {  
+        res.writeHead(404);  
+        res.write(error);  
+        res.end();  
+      } else {  
+        res.writeHead(200, {  
+              'Content-Type': 'text/html'  
+          });  
+        res.write(data);  
+        res.end();  
+      }  
+  });
+};
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 var url = require('url');  
-var fs = require('fs');  
+const createSocketIo = require('socket.io');
 
 const World = require('index-ecs').World;
 const kickoff = require("./loop");
+const httpRequestHandler = require("./httpRequestHandler");
 
 gameInit = ()=>{
   
@@ -15,35 +16,22 @@ gameLoopCallback = (delta)=>{
 }
 
 //Create the loop function by calling kickoff 
-const loop = kickoff(gameInit, gameLoopCallback);
+const loop = kickoff(gameLoopCallback);
 
 //Kick off the whole thing
 loop();
-
-//Main request handler
-const requestListener = function (req, res) {
-  fs.readFile('./page.html', function(error, data) {  
-      if (error) {  
-        res.writeHead(404);  
-        res.write(error);  
-        res.end();  
-      } else {  
-        res.writeHead(200, {  
-              'Content-Type': 'text/html'  
-          });  
-        res.write(data);  
-        res.end();  
-      }  
-  });
-};
-
 
 const host = 'localhost';
 const port = 8000;
 
 //Build the server
-const server = http.createServer(requestListener);
+const server = http.createServer(httpRequestHandler);
+const io = createSocketIo(server);
+
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
